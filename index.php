cc7bb94fb84df443b5d645d9cece36a3c305381e
neelpatel65312384 <neelpatel.6531@icloud.com>
2017-12-15 15:48:57 +0530

    n

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ if(isset($_COOKIE['user']))
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Integration</title>
+    <title>Integratio</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="robots" content="all,follow">
