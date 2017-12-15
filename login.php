d7a3e066d27c08f4ec02eb31e6593fcb40710c05
prajapatimaan <34317524+prajapatimaan@users.noreply.github.com>
2017-12-15 22:34:06 +0530

    commit try

--- login.php
+++ login.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <?php
 include('db_connect.php');
 
@@ -43,33 +43,4 @@ if(isset($_POST['login']))
 	
 }
 $conn->close();
-=======
-<?php
-include('db_connect.php');
 
-if(isset($_POST['login']))
-{
-	$user=$_POST['loginUsername'];
-	$pass=$_POST['loginPassword'];
-	 $query="select email,pass,u_type from users where email='$user' AND pass='$pass'";
-	$result=mysqli_query($conn,$query);
-	 $count=mysqli_num_rows($result);
-	if($count == 1)
-	{	
-		$row=mysqli_fetch_array($result);
-		$type=$row['u_type'];
-		$cookie_value= $user;
-		setcookie("user", $cookie_value, time() + 3600);
-		setcookie("usertype", $type, time() + 3600);
-		
-		header("location:index1.html");
-	}else
-	{
-			echo "<script>alert('invalid UserName and Password')</script>";
-			header("location:index.php");
-	}
-	
-}
-$conn->close();
->>>>>>> cc7bb94fb84df443b5d645d9cece36a3c305381e
-?>
